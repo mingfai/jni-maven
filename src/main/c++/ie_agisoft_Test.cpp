a19e245ebb32e69ba95e06830af9a14bf91cd1f2
mingfai.ma <mingfai.ma@egltours.com>
2012-11-30 19:38:57 +0800

    commented printf in native

--- src/main/c++/ie_agisoft_Test.cpp
+++ src/main/c++/ie_agisoft_Test.cpp
@@ -3,11 +3,11 @@
 #include "ie_agisoft_Test.h"
 
 JNIEXPORT void JNICALL Java_ie_agisoft_Test_hello(JNIEnv *, jobject) {
-	printf("Hello World\n");
+	//printf("Hello World\n");
 #ifdef __cplusplus
-	printf("__cplusplus is defined\n");
+	//printf("__cplusplus is defined\n");
 #else
-	printf("__cplusplus is NOT defined\n");
+	//printf("__cplusplus is NOT defined\n");
 #endif
 	return;
 }
